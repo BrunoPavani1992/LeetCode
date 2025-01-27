d21742728bcacb29c8b141f9c8750ae688d5f3fd
Bruno Pavani <brunopavani1992@gmail.com>
2025-01-27 11:31:37 -0300

    new: palindrome numbe solution

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿bool IsPalindrome(int x)
+{
+    if (x < 0)
+        return false;
+    
+    var num = x;
+    var reverseNum = 0;
+
+    while (num > 0)
+    {
+        var digit = num % 10;
+        reverseNum = reverseNum * 10 + digit;
+        num /= 10;
+    }
+
+    return x / reverseNum == 1;
+}
+
+const int input = -1000;
+
+Console.WriteLine($"{input} is palindrome? " + IsPalindrome(input));
