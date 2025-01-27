efc9d9459e218988d7d9f92870c9dcf56710bea6
Bruno Pavani <brunopavani1992@gmail.com>
2025-01-27 11:20:53 -0300

    new: two sum solution

---
+++ TwoSum/TwoSum.cs
@@ -0,0 +1,20 @@
+﻿int[] TwoSum(int[] nums, int target)
+{
+    var map = new Dictionary<int, int>();
+
+    for (var i = 0; i < nums.Length; i++)
+    {
+        if (map.ContainsKey(target - nums[i]))
+            return [map[target - nums[i]], i];
+        else
+            map.TryAdd(nums[i], i);
+    }
+
+    return [];
+}
+
+var nums = new[] { 2, 7, 11, 15 };
+const int tgt = 9;
+
+var result = TwoSum(nums, tgt);
+Console.WriteLine(string.Join(",", result));
