dabcbec831a4ab355df95e20d6d77e3d70cbfe51
Bruno Pavani <brunopavani1992@gmail.com>
2025-01-28 11:14:20 -0300

    new: valid parentheses

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,49 @@
+﻿bool IsValid(string s)
+{
+    var stack = new Stack<char>();
+
+    foreach (var chr in s)
+    {
+        if (ToPush(chr))
+            stack.Push(chr);
+        else
+        {
+            if (stack.Count == 0)
+                return false;
+            
+            if (ExpectedChar(chr) != stack.Peek())
+                return false;
+            
+            stack.Pop();
+        }
+    }
+
+    return stack.Count <= 0;
+}
+
+bool ToPush(char chr)
+{
+    return chr switch
+    {
+        '(' => true,
+        '[' => true,
+        '{' => true,
+        ')' => false,
+        ']' => false,
+        _ => false
+    };
+}
+
+char ExpectedChar(char chr)
+{
+    return chr switch
+    {
+        ')' => '(',
+        ']' => '[',
+        _ => '{',
+    };
+}
+
+const string s = "()[]{}";
+
+Console.WriteLine(IsValid(s));
