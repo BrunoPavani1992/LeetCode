24d9a343c74551542629c7bcc98380532be4f8e7
Bruno Pavani <brunopavani1992@gmail.com>
2025-01-28 10:52:17 -0300

    new: longest common prefix

---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,26 @@
+﻿string LongestCommonPrefix(string[] strs)
+{
+    var prefix = strs[0];
+
+    for (var x = 1; x < strs.Length; x++)
+    {
+        if (prefix.Length > strs[x].Length)
+        {
+            prefix = prefix[..strs[x].Length];
+        }
+        
+        for (var y = 0; y < prefix.Length && y < strs[x].Length; y++)
+        {
+            if (prefix[y] == strs[x][y]) continue;
+            
+            prefix = prefix[..y];
+            break;
+        }
+    }
+
+    return prefix;
+}
+
+var strs = new string[] { "flower","flow" };
+
+Console.WriteLine(LongestCommonPrefix(strs));
