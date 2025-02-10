4b0e2dcf27760e25b6af61608b3629a3c1c74125
Bruno Pavani <brunopavani1992@gmail.com>
2025-02-10 10:39:30 -0300

    new: Remove Duplicates

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,25 @@
+﻿int RemoveDuplicates(int[] nums)
+{
+    var list = new List<int>();
+
+    foreach (var n in nums)
+    {
+        if (list.Any(x => x == n))
+            continue;
+
+        list.Add(n);
+    }
+    
+    Array.Clear(nums);
+
+    for (var i = 0; i < list.Count; i++)
+    {
+        nums[i] = list[i];
+    }
+
+    return list.Count;
+}
+
+var input = new int[] { 1, 1, 2 };
+var k = RemoveDuplicates(input);
+Console.WriteLine(k);
