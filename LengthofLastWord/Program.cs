909f53d68220e0e2524ad7c76cb83ccf79a0208d
Bruno Pavani <brunopavani1992@gmail.com>
2026-03-12 15:25:20 -0300

    First try - Length of Last Word

---
+++ LengthofLastWord/Program.cs
@@ -0,0 +1,27 @@
+﻿int LengthOfLastWord(string s) {
+    s = s.Trim();
+
+    var word = "";
+    var reset = false;
+
+    foreach (var c in s)
+    {
+        if (c.ToString() == " ")
+        {
+            reset = true;
+        }
+        else
+        {
+            if (reset)
+            {
+                reset = false;
+                word = "";
+            }
+            word += c;    
+        }
+    }
+
+    return word.Length;
+}
+
+Console.Write(LengthOfLastWord("   fly me   to   the moon  "));
