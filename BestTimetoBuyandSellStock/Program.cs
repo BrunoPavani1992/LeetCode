4b25d7cfa6b4f18d6002a9097a0606b175359ba4
Bruno Pavani <brunopavani1992@gmail.com>
2026-04-01 09:08:29 -0300

    Improved BestTimeToBuyandSellStock

--- BestTimetoBuyandSellStock/Program.cs
+++ BestTimetoBuyandSellStock/Program.cs
@@ -1,32 +1,15 @@
 ﻿int MaxProfit(int[] prices)
 {
-    var maxProfit = new int[] { 0, 0 };
+    var max = 0;
+    var min = prices[0];
 
-    for (var i = 0; i < prices.Length; i++)
+    for (var i = 1; i < prices.Length; i++)
     {
-        for (var j = i + 1; j < prices.Length; j++)
-        {
-            if (i == j)
-                continue;
-
-            if (prices[i] < prices[j])
-            {
-                var m = maxProfit[0];
-                var n = maxProfit[1];
-
-                if (prices[j] - prices[i] > prices[n] - prices[m] || (m == 0 && n == 0))
-                {
-                    maxProfit[0] = i;
-                    maxProfit[1] = j;
-                }
-            }
-        }
+        if (prices[i] < min) min = prices[i];
+        else if (prices[i] - min > max) max = prices[i] - min;
     }
 
-    if (maxProfit[0] == 0 && maxProfit[1] == 0)
-        return 0;
-
-    return maxProfit[1];
+    return max;
 }
 
 Console.WriteLine(MaxProfit([7, 1, 5, 3, 6, 4]));
